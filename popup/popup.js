08317435bcc701760f4522c8e1c122b46a05d093
Muhammad Adam <adam184688@gmail.com>
2021-12-30 02:13:26 +0500

    Specific Fixes

--- popup/popup.js
+++ popup/popup.js
@@ -20,7 +20,17 @@ const showMessage = (primaryMessage, secondaryMessage, type) => {
 
 const showSpecificMessage = (primaryMessage, secondaryMessage, type) => {
   const messageNode = getElementById('message_specific');
-  console.log(messageNode);
+  if (type == 200) messageNode.style.background = 'rgb(0 0 0 / 10%)'
+  else messageNode.style.background = 'rgb(203 32 32 / 10%)'
+  messageNode.innerHTML = primaryMessage + '<br/>' + secondaryMessage;
+  messageNode.style.display = 'flex';
+  setTimeout(() => {
+   messageNode.style.display = 'none'
+  }, 2000);
+}
+
+const showFrogMessage = (primaryMessage, secondaryMessage, type = 401) => {
+  const messageNode = getElementById('message_frog');
   if (type == 200) messageNode.style.background = 'rgb(0 0 0 / 10%)'
   else messageNode.style.background = 'rgb(203 32 32 / 10%)'
   messageNode.innerHTML = primaryMessage + '<br/>' + secondaryMessage;
@@ -35,6 +45,21 @@ const removeContactsBar = () => {
   getElementById('remainingContact_Specific').remove();
   getElementById('remaining_contacts').remove();
   getElementById('remaining_contacts_specific').remove();
+  getElementById('remainingContact_Frog').remove();
+  getElementById('remaining_contacts_frog').remove();
+}
+
+const setFreeTrialRestrictions = () => {
+  [...document.querySelector('div[data-name="specific"]').querySelectorAll('input')].forEach(item => item.setAttribute('disabled', true));
+  [...document.querySelector('div[data-name="frog"]').querySelectorAll('input')].forEach(item => item.setAttribute('disabled', true));
+  [...document.querySelector('div[data-name="frog"]').querySelectorAll('textarea')].forEach(item => item.setAttribute('disabled', true));
+  const leapfrogButton = getElementById('leap_frog')
+  leapfrogButton.innerText = 'Upgrade to access this feature!';
+  leapfrogButton.disabled = 'true';
+  const leapSpecificButton = getElementById('leap_specific')
+  leapSpecificButton.innerText = 'Upgrade to access this feature!';
+  leapSpecificButton.disabled = 'true';
+
 }
 
 // FUNCTIONS
@@ -116,10 +141,56 @@ document.getElementById('form_specific').addEventListener('submit', (e) => {
   if (!data.numberOfContacts_specific) {
     showSpecificMessage("Invalid", "Number of contacts cannot be empty!", 400);
   } else {
-
+  const numberOfContacts = getElementById('numberOfContacts_specific').value;
+  const contactsLeft = getLocal('contactsLeft');
+  if (contactsLeft < numberOfContacts) {
+    showSpecificMessage("Not Enough Contacts","Please Update Your Plan!");
+  } else {
+    chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true
+    }, async (tabs) => {
+      const res = await fetch("http://localhost:3000/saveData", {
+        "method": "POST",
+        "headers": {
+          "content-type": "application/json"
+        },
+        "body": JSON.stringify({
+          fullName: getLocal('username'),
+          company: getLocal('company'),
+          emailAddress: getLocal('emailAddress'),
+          numberOfContacts,
+          keywords: data.keywords_specific,
+          industry: data.industry,
+          geography: data.geography,
+          employeesMin: data.min_employee,
+          employeesMax: data.max_employee,
+          revenueMin: data.revenueMin,
+          revenueMax: data.revenueMax,
+          type: 'SPECIFIC',
+        })
+      })
+    });
+    showSpecificMessage("Successfully Leaped", "Check Your Email Shortly", 200)
+  }
   }
 })
 
+
+
+getElementById('connections').addEventListener('change', (e) => {
+ if (e.target.checked) {
+  getElementById('numberOfContacts').value = 1;
+  getElementById('numberOfContacts').removeAttribute('disabled');
+  getElementById('keywords').removeAttribute('disabled');
+  getElementById('keywords').value = '';
+ } else {
+  getElementById('numberOfContacts').value = 1;
+  getElementById('numberOfContacts').setAttribute('disabled', true);
+  getElementById('keywords').setAttribute('disabled', true);
+  getElementById('keywords').value = "none"
+ }
+})
 
 document.getElementById('leap').addEventListener('click', () => {
   const contactsLeft = getLocal('contactsLeft');
@@ -162,9 +233,13 @@ document.getElementById('leap').addEventListener('click', () => {
   }
 })
 
-document.getElementById('leap_frog').addEventListener('click', () => {
+document.getElementById('leap_frog').addEventListener('click', async() => {
   const text = getElementById('leapInfo').value;
-  if (!text) showFrogMessage("Text Cannot be Empty");
+  const contactsLeft = getLocal('contactsLeft');
+  const numberOfContacts = document.getElementById('numberOfContacts_Frog').value;
+  if (!numberOfContacts) showFrogMessage("Invalid", "Contacts cannot be empty", 400);
+  else if (!text) showFrogMessage("Invalid", "Text cannot be empty", 400);
+  else if (numberOfContacts > contactsLeft) showFrogMessage("Not Enough Contacts","Please Update Your Plan!", 400)
   else {
     const res = await fetch("http://localhost:3000/saveData", {
       "method": "POST",
@@ -176,53 +251,16 @@ document.getElementById('leap_frog').addEventListener('click', () => {
         company: getLocal('company'),
         emailAddress: getLocal('emailAddress'),
         text: text,
+        numberOfContacts,
         type: 'FROG'
       })
     })
+    showFrogMessage('Successfully Leaped', 'Check Email Shortly', 200);
+    
   }
   
 })
 
-document.getElementById('leap_specificss')?.addEventListener('click', () => {
-  const numberOfContacts = document.getElementById('numberOfContacts_specific').value;
-  const contactsLeft = getLocal('contactsLeft');
-  if (contactsLeft < numberOfContacts) {
-    alert("Not enough contacts Left!");
-  } else {
-    const keywords = getElementById('keywords_specific').value;
-    const geography = getElementById('geography').value;
-    const industry = getElementById('industry').value;
-    const industryArray = industry.split(',');
-    const geographyArray = geography.split(',');
-    chrome.tabs.query({
-      active: true,
-      lastFocusedWindow: true
-    }, async (tabs) => {
-      const res = await fetch("http://localhost:3000/saveData", {
-        "method": "POST",
-        "headers": {
-          "content-type": "application/json"
-        },
-        "body": JSON.stringify({
-          fullName: getLocal('username'),
-          company: getLocal('company'),
-          emailAddress: getLocal('emailAddress'),
-          numberOfContacts,
-          keywords: keywords,
-          geography: geographyArray,
-          industry: industryArray,
-          type: 'SPECIFIC',
-        })
-      })
-    });
-    document.getElementById('message_specific').style.display = 'flex'
-    setTimeout(() => {
-      document.getElementById('message_specific').style.display = 'none'
-
-    }, 1500);
-  }
-
-})
 
 // LICENSING
 const checkLicense = async () => {
@@ -239,7 +277,7 @@ const checkLicense = async () => {
   if (!licensingStatus.authorized) {
     licensingNode.innerHTML = `
 			<div style="position: fixed; z-index: 10; width: 100%; height: 100%; background: rgb(255 255 255); top: 0; left: 0;">
-				<form name="licensing" style="margin: 95px 40px 40px; zoom: 1.125; text-align: justify;">
+				<form id="licensing" style="margin: 64px 40px 40px; zoom: 1.125; text-align: justify;">
 					<div id="licensingMessage"></div>
           <div style="
           display: flex;
@@ -247,21 +285,23 @@ const checkLicense = async () => {
       ">	<img src="../theme/assets/images/leapblack.png" height= "50"/></div>
 				
 					<span class="input-label-top">Please enter your license key to activate this extension:</span><br />
-					<input type="text" name="licenseKey">
-					<button>Activate</button>
-					<span class="input-label-top" style="margin-top: 7.5px;">Don't have a license key? <a href="https://leap.green/pricing" target="_blank">Click here to get one.</a></span>
+					<input type="text" id="licenseKey">
+					<button type="submit">Activate</button>
+					<span class="input-label-top" style="margin-top: 7.5px;">Don't have a license key? Click <a href="https://leap.green/support" target="_blank">here</a></span>
+					<span class="input-label-top" style="margin-top: 7.5px;">If you would like to book a Leap Demo Click <a href="https://www.leap.green/demo" target="_blank">here</a></span>
 				</form>
 			</div>
 		`
-    document.querySelector('[name="licensing"]').addEventListener('submit', async thisEvent => {
+    document.querySelector('[id="licensing"]').addEventListener('submit', async thisEvent => {
       thisEvent.preventDefault();
+      console.log(getFormData('form#licensing'));
       licensingNode.style.display = 'none'
       const licensingStatus = await fetch("https://leap-extension.herokuapp.com/license", {
         "headers": {
           "content-type": "application/json"
         },
         "body": JSON.stringify({
-          licenseKey: getFormData('[name="licensing"]').licenseKey
+          licenseKey: getFormData('form#licensing').licenseKey
         }),
         "method": "POST",
 
@@ -279,14 +319,17 @@ const checkLicense = async () => {
       }
     });
   } else {
-    const currentLicense = licensingStatus.validLicenses['Personal_Use-Monthly'] || licensingStatus.validLicenses['Free_Trial-Monthly'] || licensingStatus.validLicenses['Full_Access-Monthly'] || licensingStatus.validLicenses['Personal_Use-Yearly'] || licensingStatus.validLicenses['Free_Trial-Yearly'] || licensingStatus.validLicenses['Full_Access-Yearly'];
+    const currentLicense = licensingStatus.validLicenses['Personal_Use-Monthly'] || licensingStatus.validLicenses['Free_Trial'] || licensingStatus.validLicenses['Full_Access-Monthly'] || licensingStatus.validLicenses['Personal_Use-Yearly'] || licensingStatus.validLicenses['Full_Access-Yearly'];
     getElementById('remaining_contacts').innerText += ": " + currentLicense.contactsLeft;
     getElementById('remaining_contacts_specific').innerText += ": " + currentLicense.contactsLeft;
-    let percentage = ((currentLicense.contactsLeft / 100) * CONTACTS.Personal) * 100;
+    getElementById('remaining_contacts_frog').innerText += ": " + currentLicense.contactsLeft;
+    let percentage = currentLicense.contactsLeft;
     if (!currentLicense.feature) {
-      document.querySelector('div[data-name="specific"]').classList.add('blur');
+      setFreeTrialRestrictions()
+      document.querySelector('div[data-name="specific"]').setAttribute('disabled', true);
       percentage = ((currentLicense.contactsLeft / 100) * CONTACTS.Free_Trial) * 100;
     }
+    console.log(percentage);
     [...document.getElementsByClassName('progress')].forEach(item => item.style.width = `${percentage}%`)
 
     setLocal('contactsLeft', currentLicense.contactsLeft);
