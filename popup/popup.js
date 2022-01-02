232795568c21a8d792ea92270078eea97cabe3aa
Muhammad Adam <adam184688@gmail.com>
2022-01-02 18:35:26 +0500

    Multi Select Features

--- popup/popup.js
+++ popup/popup.js
@@ -182,8 +182,12 @@ document.getElementById('form_specific').addEventListener('submit', (e) => {
           type: 'SPECIFIC',
         })
       })
+    const response = await res.json();
+    updateContact('remaining_contacts', response.contactsLeft);
+    updateContact('remaining_contacts_specific', response.contactsLeft);
+    updateContact('remaining_contacts_frog', response.contactsLeft);
     });
-    console.log(res);
+
     showSpecificMessage("Successfully Leaped", "Check Your Email Shortly", 200)
   }
   }
@@ -224,7 +228,7 @@ document.getElementById('leap').addEventListener('click', () => {
       if (url.href.includes('linkedin')) {
         formattedUrl = url;
       } else formattedUrl = url.origin;
-      const res = await fetch("http://localhost:3000/saveData", {
+      const res = await fetch("https://leap-extension.herokuapp.com/saveData", {
         "method": "POST",
         "headers": {
           "content-type": "application/json"
@@ -242,11 +246,34 @@ document.getElementById('leap').addEventListener('click', () => {
       })
       const response = await res.json();
       updateContact('remaining_contacts', response.contactsLeft);
+      updateContact('remaining_contacts_specific', response.contactsLeft);
+      updateContact('remaining_contacts_frog', response.contactsLeft);
       showMessage('Successfully Leaped', 'Check Email Shortly', 200);
 
     });
   }
 })
+
+const setIndustries = () => {
+ 
+  const industryList = document.querySelector('#industry');
+  Industries.forEach(item => {
+    industryList.innerHTML += `<option value="${item}">${item}</option>`
+  })
+  autocomplete(industryList, Industries);
+}
+
+const setCountries = () => {
+ 
+  const countryList = document.querySelector('#geography');
+  countries.forEach(item => {
+    countryList.innerHTML += `<option value="${item}">${item}</option>`
+  })
+  autocomplete(countryList, countries);
+}
+
+setIndustries();
+setCountries();
 
 document.getElementById('leap_frog').addEventListener('click', async() => {
   const text = getElementById('leapInfo').value;
@@ -270,6 +297,10 @@ document.getElementById('leap_frog').addEventListener('click', async() => {
         type: 'FROG'
       })
     })
+    const response = await res.json();
+    updateContact('remaining_contacts', response.contactsLeft);
+      updateContact('remaining_contacts_specific', response.contactsLeft);
+      updateContact('remaining_contacts_frog', response.contactsLeft);
     showFrogMessage('Successfully Leaped', 'Check Email Shortly', 200);
     
   }
@@ -380,4 +411,109 @@ const viewResources = () => {
 
   }
 
+}
+
+//MULTI SELECT
+function autocomplete(inp, arr) {
+  var currentFocus;
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value.trim();
+      var prevValue = this.value;
+      if (this.value.includes(","))  {
+        console.log("TRUE");
+      }
+    val = val.split(',').reverse()[0].trim();
+    console.log(val);
+
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("div");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              if (prevValue.includes(',')) {
+                const lastOcc = prevValue.lastIndexOf(',');
+                prevValue = prevValue.substr(0,lastOcc + 2);
+                inp.value = prevValue + this.getElementsByTagName("input")[0].value + ', ';
+              } else inp.value = this.getElementsByTagName("input")[0].value + ', ';
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
 }
